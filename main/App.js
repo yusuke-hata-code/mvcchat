bc4be8720dd1f1b544ff09f83a9286d82856f4b5
yusuke-hata-code <yusuke0972@gmail.com>
2021-06-03 18:29:54 +0900

    bodyを送信できた

--- main/App.js
+++ main/App.js
@@ -1,19 +1,28 @@
+/*class EventTarget extends EventTarget {
+  constructor() {
+    super();
+    this.listener = new Map();
+  }
+}*/
+
 export class App {
   mount() {
     const formElement = document.querySelector('#form');
     const inputElement = document.querySelector('#form-input');
-    console.log(formElement.value);
+    const inputbody = document.getElementById('form-input');
 
     const socket = new WebSocket('ws://10.1.234.2:8888');
     // 接続が開いたときのイベント
-    socket.addEventListener('open', function (event) {
-      socket.send(JSON.stringify({ body: '', to: '*' }));
+    socket.addEventListener('open', function (event) {});
+
+    formElement.addEventListener('submit', (event) => {
+      event.preventDefault();
+      socket.send(JSON.stringify({ body: `${inputbody.value}`, to: '*' }));
     });
     // メッセージの待ち受け
     socket.addEventListener('message', function (event) {
       console.log('Message from server ', event.data);
     });
-    //event.preventDefault();
     console.log(`${inputElement.value}`);
   }
 }
